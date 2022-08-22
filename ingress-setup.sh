80baf0a6453a92ea261d9a7fcf956e67538cdb58
Sam Yerkes <github@yerkes.me>
2022-08-22 16:33:40 -0400

    yolo

---
+++ ingress-setup.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Demo Kind Cluster Configuration
